2c2e619a284b17abb37a0d0b768f1b4ac1b9af44
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2022-02-07 16:12:50 +0200

    adding the dependencies and the routesjs

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React , {component} from "react";
+import { Switch, Route, BrowserRouter } from "react-route-dom";
+
+class Routes extends component {
+    render(){
+        return(
+        <BrowserRouter>
+        
+        </BrowserRouter>
+        )
+    }
+}
+
+export default Routes;
