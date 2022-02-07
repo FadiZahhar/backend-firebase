c8945c097994a42a8bf2351874ca72366f507a5e
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2022-02-07 16:18:52 +0200

    adding the header footer component files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React , {component} from "react";
 import { Switch, Route, BrowserRouter } from "react-route-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 class Routes extends component {
     render(){
         return(
         <BrowserRouter>
-        
+            <Header />
+
+            <Footer />
         </BrowserRouter>
         )
     }
