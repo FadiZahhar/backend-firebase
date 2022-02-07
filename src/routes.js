5f56a388d21a7f2f8668a7aa92ceeda9a5bab310
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2022-02-07 16:41:04 +0200

    finishing the routes file and adding the html of header and footer of the back end application

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,21 @@ import { Switch, Route, BrowserRouter } from "react-route-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
+import Login from "./components/login";
+import Contact from "./components/contact";
+import Home from "./components/home";
+
+
 class Routes extends component {
     render(){
         return(
         <BrowserRouter>
             <Header />
+            <Switch>
 
+                <Route path="/contact" component={Contact} />
+                <Route path="/" component={Home} />
+            </Switch>
             <Footer />
         </BrowserRouter>
         )
